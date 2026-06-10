f46f2ef54e0b4c8459e1494ea3119de783bc8dcd
ritesh-kumar-delabs <ritesh.kumar@dancingelephantlabs.com>
2026-06-10 10:38:08 +0530

    Intial commit

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    bio : {
+        type : String,
+        default : ""
+    },
+    city : {
+        type : String,
+        default : ""
+    },
+    skillsOffered : [String],
+    skillsWanted : [String],
+    rating : {
+        type : Number,
+        default : 0
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model("User",userSchema);
