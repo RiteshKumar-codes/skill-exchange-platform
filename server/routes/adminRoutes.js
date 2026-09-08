6d0b63f364449b2640f925266c780aa996934125
RiteshKumar-codes <riteshk7710@gmail.com>
2026-09-08 16:00:14 +0530

    feat(auth): add role based authorization

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -4,11 +4,23 @@ const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
 const isAdmin = require("../middleware/adminMiddleware");
+const authorizeRoles = require("../middleware/roleMiddleware");
 
 const {getStats, getAllUsers, deleteuser} = require("../controllers/adminController");
 
 router.get("/stats",protect,isAdmin,getStats);
 router.get("/users",protect,isAdmin,getAllUsers);
 router.delete("/users/:id",protect,isAdmin,deleteuser);
+router.get("/dashboard", protect, authorizeRoles("admin"), (req, res) => {
+    res.status(200).json({
+        message: "Welcome to Admin Dashboard",
+        admin: {
+            id: req.user._id,
+            name: req.user.name,
+            email: req.user.email,
+            role: req.user.role
+        }
+    });
+});
 
 module.exports = router;
